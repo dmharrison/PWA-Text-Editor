21c919e255d9586d5b8a9080155eaec1d6f5dd71
dmharrison <dmharrison2014@gmail.com>
2024-05-30 20:44:03 -0400

    added webpack to create a manifest.json file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -30,6 +30,27 @@ module.exports = () => {
         swSrc: "./src-sw.js",
         swDest: "src-sw.js",
       }),
+
+      // Creates a manifest.json file.
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "Just Another Text Editor",
+        short_name: "J.A.T.E",
+        description: "A simple text editor",
+        favicon: "./favicon.ico",
+        background_color: "#faf8f5",
+        theme_color: "#1F1F1F",
+        start_url: "/",
+        publicPath: "/",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
     ],
 
     module: {
